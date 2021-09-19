1b933f8b1de54f87cae0a26d665e7b3c3d56f92c
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2021-09-19 17:41:55 -0300

    Add files via upload

---
+++ Banco.java
@@ -0,0 +1,20 @@
+import com.aula.poo.classes.Cliente;
+import com.aula.poo.classes.ContaCorrente;
+
+public class Banco {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("Fernando");
+        c1.setContaCorrente(new ContaCorrente(44665,6633));
+        c1.setContaCorrente(new ContaCorrente(89667,6441));
+        c1.operar();
+        Cliente c2 = new Cliente("Lucas");
+        c2.setContaCorrente(new ContaCorrente(37865,1375));
+        c2.setContaCorrente(new ContaCorrente(46913,9543));
+        c2.operar();
+        Cliente c3 = new Cliente("Lucas");
+        c3.setContaCorrente(new ContaCorrente(89633,4236));
+        c3.setContaCorrente(new ContaCorrente(10983,6542));
+        c3.operar();
+
+    }
+}
